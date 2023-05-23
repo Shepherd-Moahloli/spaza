3eb77537c7fdd8e7b36c9e10b0820a7981128b2b
Earl <shepherdmoahloli122@gmail.com>
2023-05-23 10:08:30 +0200

    "The values are now showing"

--- js/main.js
+++ js/main.js
@@ -8,10 +8,11 @@ const starch = document.getElementById("starch");
 const meat = document.getElementById("meat");
 
 meat.addEventListener("change", function (event) {
+  document.getElementById("meat-cost").value = meat.value;
   //
-  console.log("You are 4 quaters, you will change for a meat");
+  console.log("meat.value");
 });
 
 starch.addEventListener("change", function (event) {
-  console.log("You are  4 quaters, you will change for a starch");
+  console.log("starch.value");
 });

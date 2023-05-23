f95dc70324b55ad6395c9c87d0c6b0e1d3426f2f
Earl <shepherdmoahloli122@gmail.com>
2023-05-23 10:30:28 +0200

    This is coming alomng together

--- js/main.js
+++ js/main.js
@@ -7,12 +7,34 @@ const starch = document.getElementById("starch");
 
 const meat = document.getElementById("meat");
 
+const payment = document.getElementById("paid");
+
 meat.addEventListener("change", function (event) {
   document.getElementById("meat-cost").value = meat.value;
+  runThisTown();
   //
   console.log("meat.value");
 });
 
 starch.addEventListener("change", function (event) {
+  document.getElementById("starch-cost").value = starch.value;
+  runThisTown();
   console.log("starch.value");
 });
+
+payment.addEventListener("change", function (event) {
+  runThisTown();
+});
+
+function runThisTown() {
+  total =
+    parseInt(document.getElementById("meat-cost").value) +
+    parseInt(document.getElementById("starch-cost").value);
+
+  paid = parseInt((document.getElementById("paid").value = total));
+
+  document.getElementById("total").value = total;
+  document.getElementById("change").value = paid - total;
+}
+
+runThisTown();
